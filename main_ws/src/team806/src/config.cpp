47f541186b371f062705bd65365c5fe804cd2d13
Viet-Anh Nguyen <vietanh@vietanhdev.com>
2019-10-13 14:07:06 +0700

    Clean Up!

--- main_ws/src/team806/src/config.cpp
+++
@@ -1,4 +0,0 @@
-#include "config.h"
-
-std::shared_ptr<Config> Config::default_instance;
-std::string Config::racecar_pkg_name;
--- main_ws/src/team806/src/traffic_sign/traffic_sign.cpp
+++
@@ -1,7 +0,0 @@
-#include "traffic_sign.h"
-
-TrafficSign::TrafficSign(int id, cv::Rect rect) {
-    this->id = id;
-    this->rect = rect;
-    this->observe_time = Timer::getCurrentTime();
-}
