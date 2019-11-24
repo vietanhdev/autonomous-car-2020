0a8dd40466c4ee341f1c20f0fb64dd2ae3fb369a
Viet Anh Nguyen <vietanh@vietanhdev.com>
2019-11-24 14:46:55 +0700

    Add node to keep simulator running

--- main_ws/src/teamict/src/keep_running_node.py
+++ main_ws/src/teamict/src/keep_running_node.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+
+'''
+This node publish a speed = 0 when not hearing from speed topic for a duration.
+This trick prevent simulator from not sending image to our control node
+'''
+
 from __future__ import print_function
 import time
 import config
@@ -6,6 +12,11 @@ import roslib
 import rospy
 from std_msgs.msg import Float32
 
+
+last_time_set_speed = 0
+def set_speed_received(data):
+    global last_time_set_speed
+    last_time_set_speed = time.time()
 
 if __name__ == '__main__':
 
@@ -15,10 +26,13 @@ if __name__ == '__main__':
     # Save to restart transfering images
     speed_pub = rospy.Publisher(config.TOPIC_SET_SPEED, Float32, queue_size=1)
 
-    r = rospy.Rate(0.2) #10hz
+    rospy.Subscriber(config.TOPIC_SET_SPEED, Float32, set_speed_received, queue_size=1)
+
+    r = rospy.Rate(0.5)
 
     while not rospy.is_shutdown():
-        speed_pub.publish(0)
+        if time.time() - last_time_set_speed > 2:
+            speed_pub.publish(0)
         r.sleep()
 
 
